38dd4b49ca0e7e72431bc77f8fb8ad2222a08194
LaPierre, Jason <jason.lapie@gmail.com>
2026-04-01 14:09:17 -0400

    Added TailwindCSS via Vite and Futura Font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
@@ -16,5 +16,8 @@ export default defineConfig({
               /^\/api\/.+/
           ]
       }
-  })
+  }),
+  vite: {
+      plugins: [tailwindcss()],
+  },
 });
